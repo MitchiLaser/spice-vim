07de78f6839aa1b0b27422f79cb23511ac0338e8
Michael Hohenstein <michael@hohenste.in>
2023-08-30 14:09:33 +0200

    install script now checking for an existing vim and git installation

--- install.sh
+++ install.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
+# check for an existing vim installation
+if ! (command -v vim >/dev/null 2>&1); then
+	echo "Error: You have to install vim first" >&2
+	exit 1
+fi
+
+# check for an existing git installation
+if ! (command -v git >/dev/null 2>&1); then
+	echo "Error: You have to install git first" >&2
+	exit 1
+fi
+
 # install vim-plug
 # this line was copied from the vimplug README file
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+
 # create ~/.vimrc with the content of the vimrc file in this directory
-if [[ ~/.vimrc  ]]
+if [[ ~/.vimrc  ]]; then
 	echo "~/.vimrc found, appending configuration to this file"
 	echo $(dirname)/vimrc >> ~/.vimrc
 else
 	echo "Installing ~/.vimrc"
 	cp $(dirname)/vimrc ~/.vimrc
 fi
+
 
 # start vim, install the plugins and quit it again
 vim +PlugInstall +qall
